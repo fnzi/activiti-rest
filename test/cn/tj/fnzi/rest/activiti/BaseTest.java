a38c26c1184feaefcee9df610a2db54f0ecb7d40
Administrator <Administrator@PC-20120117GSYE>
2014-09-25 23:42:00 +0800

    activiti rest

---
+++ test/cn/tj/fnzi/rest/activiti/BaseTest.java
@@ -0,0 +1,11 @@
+package cn.tj.fnzi.rest.activiti;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContext-test.xml" })
+public class BaseTest {
+
+}
---
+++ test/cn/tj/fnzi/rest/activiti/Test001.java
@@ -0,0 +1,64 @@
+package cn.tj.fnzi.rest.activiti;
+
+import it.sauronsoftware.base64.Base64;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Request;
+import org.junit.Test;
+
+public class Test001 extends BaseTest {
+
+    // @Test
+    // public void test001() throws Exception {
+    //
+    // DefaultHttpClient httpClient = new DefaultHttpClient();
+    // HttpPost postRequest = new
+    // HttpPost("http://localhost:8080/activiti-rest/service/login");
+    //
+    // StringEntity input = new
+    // StringEntity("{\"userId\":\"kermit\",\"password\":\"kermit\"}");
+    // input.setContentType("application/json");
+    // postRequest.setEntity(input);
+    //
+    // HttpResponse response = httpClient.execute(postRequest);
+    //
+    // BufferedReader br = new BufferedReader(new
+    // InputStreamReader((response.getEntity().getContent())));
+    //
+    // String output;
+    // System.out.println("Output from Server .... \n");
+    //
+    // while ((output = br.readLine()) != null) {
+    // System.out.println(output);
+    // }
+    //
+    // httpClient.getConnectionManager().shutdown();
+    //
+    // }
+
+    @Test
+    public void test002() throws Exception {
+        try {
+            // String getUsersUrl =
+            // "http://localhost:8080/activiti-rest/service/identity/users";
+//            String getTablesUrl = "http://localhost:8080/activiti-rest/service/management/tables";
+            String getTablesUrl = "http://localhost:8080/activiti-rest/service/management/tables/ACT_RE_PROCDEF";
+            String authorization = "Basic " + Base64.encode("kermit:kermit", "utf-8");
+
+            String ret = Request.Get(getTablesUrl).addHeader("Authorization", authorization).execute().returnContent().asString();
+
+            System.out.println(ret);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // @Test
+    // public void testBase64() {
+    // System.out.println(Base64.encode("kermit:kermit", "utf-8"));
+    // }
+}
